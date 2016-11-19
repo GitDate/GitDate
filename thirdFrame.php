7a8fcfcb136f51e80812a82fde2d0610391737bc
Aung Myat <amyat@purdue.edu>
2016-11-19 15:59:01 -0600

    Flies remained to push

---
+++ thirdFrame.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<p>Welcome <?php echo $_POST['answer1']; ?></p>
+<p>Welcome <?php echo $_POST['answer2']; ?></p>
+<p>Welcome <?php echo $_POST['answer3']; ?></p>
+</body>
+</html>
