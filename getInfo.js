9e228ba8558e90a89ba648e2d2a241bc4013873d
Aung Myat <amyat@purdue.edu>
2016-11-19 22:00:11 -0600

    login verification done

--- getInfo.js
+++ getInfo.js
@@ -1,10 +1,31 @@
-function myFunction() {
-    var x = document.getElementById("frm1");
-    var text = "";
-    var i;
-    for (i = 0; i < x.length ;i++) {
-        text += x.elements[i].value + "<br>";
+$(document).ready(function(){
+	$('#loginSubmit').click(function(event){
+		event.preventDefault();
+		var x = $('#email').val();
+		var y = $('#pwd').val();
+
+		readTextFile('database.txt', x, y);
+	});
+});
+
+function readTextFile(file, x, y) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function (){
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+            	var format = x + ':' + y;
+                var allText = rawFile.responseText;
+                if(allText.match(format)) {
+                	console.log('success');
+                	$('#login').submit();
+                } else {
+                	console.log('error');
+                }
+            }
+        }
     }
-    document.getElementById("demo").innerHTML = text;
-    console.log(text);
+    rawFile.send(null);
 }
